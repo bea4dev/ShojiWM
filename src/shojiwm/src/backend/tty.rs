3974eec70571e5422863b34160f1889915e74f9b
Seirra <sophietheopossum@yandex.ru>
2026-08-18 17:55:11 +0100

    collapse if statements

--- src/shojiwm/src/backend/tty.rs
+++ src/shojiwm/src/backend/tty.rs
@@ -12972,8 +12972,8 @@ fn connector_disconnected(
         color_state
     ) = state.output_color.remove(
         &output_name
-    ) {
-        if let Some(
+    )
+        && let Some(
             blob
         ) = color_state.hdr_metadata_blob {
             // The kernel only frees property blobs when the DRM fd closes,
@@ -12985,7 +12985,6 @@ fn connector_disconnected(
                 blob,
             );
         }
-    }
     let output = surface.output;
     state.space.unmap_output(&output);
     state.remove_output_global(&output);
